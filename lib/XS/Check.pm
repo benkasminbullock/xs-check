a401e1dee3d3035b72376f6b7c3a2d785fba9278
Ben Bullock <benkasminbullock@gmail.com>
2017-08-20 11:11:50 +0900

    Remove Exporter stuff

--- lib/XS/Check.pm
+++ lib/XS/Check.pm
@@ -3,12 +3,6 @@ use warnings;
 use strict;
 use Carp;
 use utf8;
-require Exporter;
-# our @ISA = qw(Exporter);
-# our @EXPORT_OK = qw//;
-# our %EXPORT_TAGS = (
-#     all => \@EXPORT_OK,
-# );
 our $VERSION = '0.02';
 use C::Tokenize ':all';
 use Text::LineNumber;
